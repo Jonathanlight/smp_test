570e14ccdb81229b9711b520289689b99ffafa84
jkablan <jkablan@kernix.com>
2019-06-04 15:58:13 +0200

    extension admin

--- app/symfony/src/Twig/AdminExtension.php
+++ app/symfony/src/Twig/AdminExtension.php
@@ -15,6 +15,7 @@ class AdminExtension extends AbstractExtension
     {
         return [
             new TwigFilter('filter_admin_actions', [$this, 'filterActions']),
+            new TwigFilter('filter_admin_edit', [$this, 'filterEdit']),
         ];
     }
 
@@ -31,5 +32,20 @@ class AdminExtension extends AbstractExtension
         }
 
         return $actions;
+    }
+
+    /**
+     * @param array $actions
+     * @param mixed $item
+     *
+     * @return string
+     */
+    public function filterEdit(array $actions, $item): string
+    {
+        if ($item instanceof Center && $item->getCourses()->count() > 10) {
+            return "first run ...";
+        }
+
+        return "second run ...";
     }
 }
